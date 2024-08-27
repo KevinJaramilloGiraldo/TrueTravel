5f04e307c0d3b53cd00c552261acc0fd0ff5bfb1
Leonard <Leonardhernandez02@gmail.com>
2024-08-27 17:02:22 -0500

    creando las variables que se usaran para implementar los demas metodos

---
+++ true_travel.py
@@ -0,0 +1,16 @@
+#variables compartidas
+cliente1 = {'nombre': 'leonard', 'apellido': 'hernandez'}
+vuelo1 = {'origen': 'londres', 'destino': 'nueva york'}
+hotel1 = {'nombre': 'hotel bolivar', 'ciudad': 'londres'}
+
+lista_clientes = [cliente1]
+lista_vuelos = [vuelo1]
+lista_hoteles =[hotel1]
+
+reserva = {'cliente': cliente1, 'vuelo': vuelo1, 'hotel': hotel1, 'vuelo_regreso': vuelo1}
+paquete_turistico = {"ciudad": 'londres', 'vuelo_ida': vuelo1, 'vuelo_regreso': vuelo1, 'hotel': hotel1}
+
+lista_reservas = [reserva]
+paquete_turisticos = [paquete_turistico]
+
+
