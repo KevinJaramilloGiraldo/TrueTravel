dd1b4a09dc66c75ec527308d82e2722f87e0b416
KevinJaramilloGiraldo <kevin.jaramillo@correounivalle.edu.co>
2024-08-31 19:54:52 -0500

    Implementación de las metodos de cientes service y solución de errores descubiertos con las pruebas

--- model/cliente.py
+++ model/cliente.py
@@ -1,11 +1,11 @@
-class cliente:
+class Cliente:
     # Variable de clase para el contador de IDs
     _id_counter = 1
     
     def __init__(self, nombre, apellido):
-        self.id = cliente._id_counter
+        self.id = Cliente._id_counter
         self.nombre = nombre
         self.apellido = apellido
         
         # Incrementar el contador de IDs para el siguiente cliente
-        cliente._id_counter += 1
+        Cliente._id_counter += 1
